0e012821ea8bdcc6836dc4f7c460ae31ca3c3f5a
Arij Thabet <arijthabet01@gmail.com>
2025-02-23 01:17:30 +0100

    ui corrected

--- StripeMetrics-Frontend/src/pages/dashboard/Dashboard.tsx
+++ StripeMetrics-Frontend/src/pages/dashboard/Dashboard.tsx
@@ -25,21 +25,24 @@ export const Dashboard: React.FC = () => {
 
   return (
     <div className="bg-gray-100 w-full min-h-screen flex flex-col items-center p-6">
+    {/* <div className="w-full h-screen flex flex-col items-center justify-center align-center px-4 bg-gradient-to-b from-white to-gray-300"> */}
       {/* Header Section */}
       <div className="w-full max-w-6xl bg-white shadow-lg rounded-xl p-6 flex flex-col lg:flex-row items-center justify-between">
-        <h2 className="text-3xl font-bold text-blue-600">Upload Your Label</h2>
+        <div className="flex flex-row items-center ">
+        <img src="assets/img/clothes.png" className="w-10 mr-2"></img>
+        <h2 className="text-3xl font-bold text-black">DefectUs</h2></div>
         <div className="flex items-center space-x-4 mt-4 lg:mt-0">
-          <Datepicker value={value} onChange={handleValueChange}  />
+          {/* <Datepicker value={value} onChange={handleValueChange}  /> */}
           <button
             onClick={appLogout}
-            className="flex items-center bg-gray-200 hover:bg-blue-600 text-blue-600 hover:text-white px-4 py-2 rounded-lg shadow transition"
+            className="flex items-center bg-gray-200 hover:bg-black text-black hover:text-white px-4 py-2 rounded-lg shadow transition"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke="currentColor"
+              stroke="black"
               className="w-6 h-6 mr-2"
             >
               <path
