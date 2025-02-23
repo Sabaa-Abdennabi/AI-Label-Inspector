372ce899ca4d64cb235731b7487db0d9256f44c5
Arij Thabet <arijthabet01@gmail.com>
2025-02-23 01:17:30 +0100

    ui corrected

--- StripeMetrics-Frontend/src/pages/home/Home.tsx
+++ StripeMetrics-Frontend/src/pages/home/Home.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Home: React.FC = () => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center align-center px-4">
-      <div className="relative">
-        <img className="xl:w-[36px] w-[24px] xl:h-[36px] h-[24px] absolute xl:right-[-40px] right-[-30px] xl:top-[-20px] top-[-12px]" src="assets/img/stripe-round-logo.png" alt="Stripe" />
-        <h1 className="xl:text-7xl text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-indigo-600 drop-shadow-lg shadow-slate-500">StripeMetrics</h1>
-      </div>
-      <div className='py-16 flex flex-row space-x-2'>
-        <Link to="/signup">
-          <button className='bg-white hover:bg-indigo-500 px-5 py-2 text-sm leading-5 rounded-md font-semibold text-indigo-500 hover:text-white border border-indigo-500 hover:border-indigo-500 w-32'>
-            Sign up
-          </button>
-        </Link>
-        <Link to="/login">
-          <button className='bg-indigo-500 hover:bg-indigo-700 px-5 py-2 text-sm leading-5 rounded-md font-semibold text-white border border-indigo-500 hover:border-indigo-700 w-32'>
-            Log in
-          </button>
-        </Link>
+    <div className="w-full h-screen flex flex-col items-center justify-center align-center px-4 bg-gradient-to-b from-white to-gray-300">
+      <div className="relative bg-white bg-opacity-50 backdrop-blur-md p-8 rounded-lg shadow-lg">
+        <img
+          className="w-[36px] h-[36px] absolute xl:right-[50px]  top-[20px]"
+          src="assets/img/tag.png"
+          alt="Stripe"
+        />
+        <h1 className="xl:text-7xl text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r text-black drop-shadow-lg shadow-slate-500">
+          DefectUs
+        </h1>
+        <h3 className="mt-7">
+          Precision in Every Label, Efficiency in Every Scan
+        </h3>
+
+        <div className="py-10 flex flex-row space-x-2 align-middle justify-center">
+          <Link to="/signup">
+            <button className="bg-white hover:bg-black px-5 py-2 text-sm leading-5 rounded-md font-semibold text-black hover:text-white border border-black hover:border-black w-32">
+              Sign up
+            </button>
+          </Link>
+          <Link to="/login">
+            <button className="bg-black hover:bg-gray-700 px-5 py-2 text-sm leading-5 rounded-md font-semibold text-white border border-black hover:border-black w-32">
+              Log in
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
-}
+};
